847e136e90a8a69e8660fdefa611198153ab2652
Hillworks <connor@hillworks.co.uk>
2025-07-28 16:53:47 +0100

    Update binary_sensor.py

--- custom_components/ambient_music/binary_sensor.py
+++ custom_components/ambient_music/binary_sensor.py
@@ -1,54 +1,49 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN, CONF_PLAYLISTS
+from .const import DOMAIN, CONF_PLAYLISTS, DEVICE_INFO
 
-class PlaylistSelectedSensor(BinarySensorEntity):
-    def __init__(self, playlist_name: str, input_select_entity: str):
+SELECT_ENTITY_ID = "select.ambient_music_playlist"
+
+class PlaylistEnabledSensor(BinarySensorEntity):
+    def __init__(self, hass: HomeAssistant, playlist_name: str):
+        self.hass = hass
         self._playlist_name = playlist_name
-        self._input_select_entity = input_select_entity
-        self._attr_name = f"Playlist: {playlist_name} Selected"
-        self._attr_unique_id = f"{input_select_entity}_{playlist_name.replace(' ', '_').lower()}_selected"
+        self._attr_name = f"{playlist_name} Enabled"
+        self._attr_unique_id = f"ambient_music_{DOMAIN}_{playlist_name.lower().replace(' ', '_')}_enabled"
         self._attr_is_on = False
 
     async def async_added_to_hass(self):
         self._update_state()
 
         @callback
-        def handle_state_change(event):
+        def handle_select_change(event):
             self._update_state()
 
-        async_track_state_change_event(
-            self.hass, [self._input_select_entity], handle_state_change
-        )
+        async_track_state_change_event(self.hass, SELECT_ENTITY_ID, handle_select_change)
 
     def _update_state(self):
-        current = self.hass.states.get(self._input_select_entity)
-        if current and current.state == self._playlist_name:
-            self._attr_is_on = True
-        else:
-            self._attr_is_on = False
+        state = self.hass.states.get(SELECT_ENTITY_ID)
+        self._attr_is_on = state and state.state == self._playlist_name
         self.async_write_ha_state()
 
     @property
     def is_on(self):
         return self._attr_is_on
+        
+    @property
+    def device_info(self):
+        return DEVICE_INFO
 
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
+    playlists = entry.data.get(CONF_PLAYLISTS, [])
+    if isinstance(playlists, str):
+        playlists = playlists.splitlines()
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
-    data = entry.data
-    input_select_entity = data.get(CONF_PLAYLISTS)
-    playlist_state = hass.states.get(input_select_entity)
-
-    if not playlist_state or not hasattr(playlist_state, "attributes"):
-        return
-
-    playlist_options = playlist_state.attributes.get("options", [])
-    entities = [
-        PlaylistSelectedSensor(name, input_select_entity)
-        for name in playlist_options
-    ]
-    async_add_entities(entities)
+    sensors = [PlaylistEnabledSensor(hass, name) for name in playlists]
+    async_add_entities(sensors)
