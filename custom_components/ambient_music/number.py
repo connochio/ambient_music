b60dada4f61034d76c896d5690d02e1e0d9e5643
Hillworks <connor@hillworks.co.uk>
2025-07-28 12:34:08 +0100

    Update number.py

--- custom_components/ambient_music/number.py
+++ custom_components/ambient_music/number.py
@@ -2,6 +2,7 @@ from homeassistant.components.number import NumberEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity import EntityCategory
 
 from .const import DOMAIN, DEVICE_INFO
 
@@ -13,12 +14,6 @@ NUMBER_ENTITIES = [
     ("Volume Fade Up Seconds", 0, 20, 1)
 ]
 
-async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
-):
-    entities = [AmbientMusicNumber(name, min_val, max_val, step) for name, min_val, max_val, step in NUMBER_ENTITIES]
-    async_add_entities(entities)
-
 class AmbientMusicNumber(NumberEntity):
     def __init__(self, name, min_val, max_val, step):
         self._attr_name = f"Ambient Music {name}"
@@ -28,7 +23,17 @@ class AmbientMusicNumber(NumberEntity):
         self._attr_native_step = step
         self._attr_native_value = min_val
         self._attr_mode = "auto"
+    
+    def set_native_value(self, value):
+        self._attr_native_value = value
+        self.async_write_ha_state()
 
     @property
     def device_info(self):
         return DEVICE_INFO
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
+    entities = [AmbientMusicNumber(name, min_val, max_val, step) for name, min_val, max_val, step in NUMBER_ENTITIES]
+    async_add_entities(entities)
