fe0bf1f9021d0330f464b6fab3c00f7a54d5581b
Hillworks <connor@hillworks.co.uk>
2025-07-28 16:55:32 +0100

    Update number.py

--- custom_components/ambient_music/number.py
+++ custom_components/ambient_music/number.py
@@ -2,7 +2,7 @@ from homeassistant.components.number import NumberEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.entity import EntityCategory
+from homeassistant.helpers.restore_state import RestoreEntity
 
 from .const import DOMAIN, DEVICE_INFO
 
@@ -14,7 +14,7 @@ NUMBER_ENTITIES = [
     ("Volume Fade Up Seconds", 0, 20, 1)
 ]
 
-class AmbientMusicNumber(NumberEntity):
+class AmbientMusicNumber(NumberEntity, RestoreEntity):
     def __init__(self, name, min_val, max_val, step):
         self._attr_name = f"Ambient Music {name}"
         self._attr_unique_id = self._attr_name.lower().replace(" ", "_")
@@ -23,10 +23,22 @@ class AmbientMusicNumber(NumberEntity):
         self._attr_native_step = step
         self._attr_native_value = min_val
         self._attr_mode = "auto"
-    
-    async def async_set_native_value(self, value: float) -> None:
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        if (restored_state := await self.async_get_last_state()) is not None:
+            try:
+                self._attr_native_value = float(restored_state.state)
+            except ValueError:
+                pass
+
+    async def async_set_native_value(self, value: float):
         self._attr_native_value = value
         self.async_write_ha_state()
+
+    @property
+    def native_value(self) -> float:
+        return self._attr_native_value
 
     @property
     def device_info(self):
