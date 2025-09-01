4de0d6563d8267d645a58ab86ce8ce53fc10e395
Hillworks <connor@hillworks.co.uk>
2025-09-01 14:35:32 +0100

    Added blocker and new config flow
    - added config flow options to create, amend and remove blockers
    - added enablement binary sensor, based off of the blockers statuses
    - updated version to 1.1.7-dev.1

--- custom_components/ambient_music/const.py
+++ custom_components/ambient_music/const.py
@@ -3,6 +3,14 @@ DOMAIN = "ambient_music"
 CONF_MEDIA_PLAYERS = "media_players"
 CONF_PLAYLISTS = "playlists"
 CONF_SPOTIFY_ID = "spotify_id"
+CONF_BLOCKERS = "blockers"
+BLOCKER_ID = "id"
+BLOCKER_NAME = "name"
+BLOCKER_TYPE = "type"
+BLOCKER_INVERT = "invert"
+BLOCKER_ENTITY_ID = "entity_id"
+BLOCKER_STATE = "state"
+BLOCKER_TEMPLATE = "template"
 
 DEVICE_INFO = {
     "identifiers": {(DOMAIN,)},
